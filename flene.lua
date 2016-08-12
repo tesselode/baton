f00ecb98bd318518f0b50a5936e5aef44ae837db
Andrew Minnich <aminnich3@gmail.com>
2016-08-12 00:45:03 -0400

    Parse input sources

--- flene.lua
+++ flene.lua
@@ -1,2 +1,25 @@
 -- PHASE 1: JUST SINGLE PLAYER
 -- Axis/button interoperability, remappable controls, keyboard + controller
+
+local flene = {}
+
+local Control = {}
+
+local function newControl(sources)
+  local control = {sources = {}}
+  for _, source in ipairs(sources) do
+    local type, value = source:match('(.*):(.*)')
+    print(type, value)
+  end
+end
+
+local Manager = {}
+
+function flene.new(controls)
+  local manager = {controls = {}}
+  for name, sources in pairs(controls) do
+    newControl(sources)
+  end
+end
+
+return flene
