f39b58cc9494ae26fb9ab7e443393c1cd4d5409f
Andrew Minnich <aminnich3@gmail.com>
2017-11-08 21:52:06 -0500

    performance stats

--- main.lua
+++ main.lua
@@ -21,7 +21,12 @@ local pairDisplayTargetAlpha = 0
 local buttonDisplayAlpha = 0
 local buttonDisplayTargetAlpha = 0
 
+local updates = 0
+local updateTime = 0
+
 function love.update(dt)
+  local time = love.timer.getTime()
+  
   player:update()
 
   pairDisplayTargetAlpha = player:pressed 'move' and 255
@@ -45,6 +50,9 @@ function love.update(dt)
   if buttonDisplayAlpha < buttonDisplayTargetAlpha then
     buttonDisplayAlpha = buttonDisplayTargetAlpha
   end
+
+  updateTime = updateTime + (love.timer.getTime() - time)
+  updates = updates + 1
 end
 
 function love.keypressed(key)
@@ -60,7 +68,9 @@ local pairDisplayRadius = 128
 
 function love.draw()
   love.graphics.setColor(255, 255, 255)
-  love.graphics.print(tostring(player:getActiveDevice()))
+  love.graphics.print('Current active device: ' .. tostring(player:getActiveDevice()))
+  love.graphics.print('Average update time (us): ' .. math.floor(updateTime/updates*1000000), 0, 16)
+  love.graphics.print('Memory usage (kb): ' .. math.floor(collectgarbage 'count'), 0, 32)
 
   love.graphics.push()
   love.graphics.translate(400, 300)
